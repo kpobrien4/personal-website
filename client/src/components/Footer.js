102efcb63d18ef4755bfe018b57daa088ce30c72
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-05 15:09:43 -0500

    finished footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import llogo from '../llogo.png'
+import glogo from '../glogo.png'
+
 
 export class Footer extends Component {
   render() {
     return (
         <footer class="page-footer light-blue darken-1">
-        <div class="container">
+        <div class="container center">
           <div class="row">
             <div class="col l6 s12">
-              <h5 class="white-text">Footer Content</h5>
-              <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-            </div>
-            <div class="col l4 offset-l2 s12">
-              <h5 class="white-text">Links</h5>
               <ul>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                <li><a href="https://www.linkedin.com/in/kpobrien4/"><img src={llogo} width="75" height="75" alt="LinkedIn"/></a></li>
+              </ul>
+            </div>
+            <div class="col l6 s12">
+              <ul>
+                <li><a href="https://github.com/kpobrien4"><img src={glogo} width="75" height="75"/></a></li>
               </ul>
             </div>
           </div>
         </div>
         <div class="footer-copyright">
           <div class="container">
-          © 2019 Copyright Text
-          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
           </div>
         </div>
       </footer>
