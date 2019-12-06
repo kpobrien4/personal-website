64f0edb3b77f29ee4faa659f373754796ef44a8e
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-06 10:53:43 -0500

    working on quote functionality

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import IMG_3226 from '../IMG_3226.PNG'
+import Quotes from '../containers/Quotes'
+
+
 
 export class Home extends Component {
   render() {
@@ -7,7 +10,10 @@ export class Home extends Component {
       <div className="container home">
         <h1 class="center "><img src={IMG_3226} width="250" height="250" alt="Me"/></h1>
         <h3 class="center black-text thin">Full Stack Web Developer</h3>
+        <br></br>
+        <Quotes />
       </div>
+      
     )
   }
 }
---
+++ client/src/containers/Quotes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Quotes extends Component {
+  render() {
+    return (
+      <div className="container quotes">
+        <p class="center"><i>"I only love my bed and my mama I'm sorry."</i> - <b>Aubrey Graham</b></p>
+      </div>
+      
+    )
+  }
+}
+
+
+export default Quotes
