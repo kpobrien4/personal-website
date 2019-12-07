01c09a413cf5a418c627b68d6b64d946ed9af358
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-07 13:39:00 -0500

    cool fade

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import IMG_3226 from '../images/IMG_3226.PNG'
 import Quotes from '../containers/Quotes'
+import '../home.css'
 
 
 
@@ -8,8 +9,8 @@ export class Home extends Component {
   render() {
     return (
       <div className="container home">
-        <h1 class="center "><img src={IMG_3226} width="250" height="250" alt="Me"/></h1>
-        <h3 class="center black-text thin">Full Stack Web Developer</h3>
+        <h1 class="center home1"><img src={IMG_3226} width="250" height="250" alt="Me"/></h1>
+        <h3 class="center black-text thin home3">Full Stack Web Developer</h3>
         <br></br>
         <Quotes />
       </div>
--- client/src/containers/Quotes.js
+++ client/src/containers/Quotes.js
@@ -4,7 +4,7 @@ export class Quotes extends Component {
   render() {
     return (
       <div className="container quotes">
-        <p class="center"><i>"I only love my bed and my mama I'm sorry."</i> - <b>Aubrey Graham</b></p>
+        <p class="center quote"><i>"I only love my bed and my mama I'm sorry."</i> - <b>Aubrey Graham</b></p>
       </div>
       
     )
