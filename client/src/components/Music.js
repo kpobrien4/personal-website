a9c018d5d393fe9365af53ab0b68b1cb18f98644
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-09 09:31:35 -0500

    added instrument list

--- client/src/components/Music.js
+++ client/src/components/Music.js
@@ -14,6 +14,19 @@ export class Music extends Component {
         </audio>
     </div>
     <hr></hr>
+    <h3 class="thin">Instruments and Technology:</h3>
+    <ul class="list browser-default thin">
+        <li>Logic Pro X</li>
+        <li>M Audio Code 61</li>
+        <li>Teenage Engineering OP-Z</li>
+        <li>AKG P420 Condenser</li>
+        <li>PreSonus AudioBox 96</li>
+        <li>Ableton</li>
+        <li>Macbook Pro</li>
+        <li>Supro 2020BM Westbury Dual Pickup Island Series Electric Guitar</li>
+        <li>Yamaha P45 Digital Piano</li>
+
+    </ul>
         <br></br>
         <br></br>
         <br></br>
