53123a732b5ec8c1edce8d7e619651b15440d635
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-09 14:19:39 -0500

    browser router maybe?

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,13 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Music from './components/Music'
 import './App.css';
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 
 class App extends Component {
   render(){
   return (
-    <HashRouter>
+    // <HashRouter hashType="noslash">
+    <Router >
       <Nav />
       <div className="container">
         <Switch>
@@ -29,7 +30,7 @@ class App extends Component {
       <br></br>
       <br></br>
       <Footer />
-    </HashRouter>
+    </Router>
   );
   }
 }
