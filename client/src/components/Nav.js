c67d4db0e44e95815758a8eaa7351eb95092ffdf
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-12 13:14:02 -0500

    mobile READY

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -15,12 +15,12 @@ export class Nav extends Component {
           <Link to="/" class="brand-logo thin black-text">Kevin O'Brien</Link>
           <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
           <ul className="right" media="(max-width: 800px)">
-            <li media="(max-width: 800px)"><Link to="/about" class="thin black-text">About</Link></li>
-            <li media="(max-width: 800px)"><Link to="/projects" class="thin black-text">Projects</Link></li>
-            <li media="(max-width: 800px)"><a href = {Pdf} target="_blank" rel="noopener noreferrer" class="thin black-text">Resume</a></li>
-            <li media="(max-width: 800px)"><a href = "https://kpobrien4.github.io/" target="_blank" rel="noopener noreferrer" class="thin black-text">Blog</a></li>
-            <li media="(max-width: 800px)"><Link to="/music" class="thin black-text">Music</Link></li>
-            <li media="(max-width: 800px)"><Link to="/contact" class="thin black-text">Contact</Link></li>
+            <li class="desktop-only"><Link to="/about" class="thin black-text">About</Link></li>
+            <li class="desktop-only"><Link to="/projects" class="thin black-text">Projects</Link></li>
+            <li class="desktop-only"><a href = {Pdf} target="_blank" rel="noopener noreferrer" class="thin black-text">Resume</a></li>
+            <li class="desktop-only"><a href = "https://kpobrien4.github.io/" target="_blank" rel="noopener noreferrer" class="thin black-text">Blog</a></li>
+            <li class="desktop-only"><Link to="/music" class="thin black-text">Music</Link></li>
+            <li class="desktop-only"><Link to="/contact" class="thin black-text">Contact</Link></li>
           </ul>
           
         </div>
