0cf312a551880adb08b9559c494ca3418a9dd437
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-14 14:03:56 -0500

    error page

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import Monkey from '../images/monkey.jpeg'
 
 export class Errors extends Component {
   render() {
     return (
-      <div>
-        This page doesn't exist.
+      <div class="center">
+        <br></br>
+        <h4 class="thin">Get a grip there, buster! This page doesn't exist</h4>
+        <br></br>
+        <img src={Monkey} width="300" height="400" alt="Me"/>
+        <h5 class="thin">Now you got my mans lookin at you like this</h5>
       </div>
     )
   }
