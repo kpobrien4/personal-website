88305c584516ff8810852edc3e3fc8c462096fca
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-17 13:48:38 -0500

    removed webpack

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import CodingStock from '../images/CodingStock.jpeg'
+import '../about.css'
 
 export class About extends Component {
   render() {
     return (
       <div>
-        <h3 class="thin">About Me</h3>
-        <p>Kevin O'Brien is a software engineer, physicist, and musician from upstate New York. </p>
+        <h3 class="thin about">About Me</h3>
+        <p>I'm a software engineer, physicist, and musician from upstate New York.  </p>
+
+        <p>My first introduction to coding came in 2013, while still in high school, with a video called "What Most Schools Don't Teach" which featured the likes of tech household names such as Bill Gates, Mark Zuckerberg and Jack Dorsey, and those less known for their coding exploits like Will.I.Am talking about their experiences with computer programming. It was immediately captivating to me
+        </p>
         <br></br>
         <br></br>
         <br></br>
