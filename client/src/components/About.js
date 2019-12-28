4fc3b7e3391f00947f4bcaab8137c2c4f0adae4f
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-28 15:54:40 -0500

    formatting

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,9 +9,9 @@ export class About extends Component {
         <div class="header">
         <h3 class="thin about">About Me</h3>
         </div>
-        <p>Hello treasured reader! My name is Kevin and I'm a software engineer, physicist, and musician from upstate New York.  </p>
+        <p class="thin">Hello treasured reader! My name is Kevin and I'm a software engineer, physicist, and musician from upstate New York. I graduated from the University at Buffalo in May of 2019 with a degree in Physics, and immediately attended Flatiron School, an immersive five month coding school in the field of software development. I have since been working on a bunch of software projects, one of which is this very website! My goal is to create software, or work for a company that creates software with the aim of helping people. I hope to be an inspiring and creative developer, who is always striving to learn and teach others. My current short term goals are to complete the courses I am enrolled in in Python and Linux</p>
 
-        <p>My first introduction to coding came in 2013, while still in high school, with a video called "What Most Schools Don't Teach" which featured the likes of tech household names such as Bill Gates, Mark Zuckerberg and Jack Dorsey, and those less known for their coding exploits like Will.I.Am talking about their experiences with computer programming. It was immediately captivating to me
+        <p class="thin">My first introduction to coding came in around 2016, right after graduating from high school, with a video called "What Most Schools Don't Teach" which featured the likes of tech household names such as Bill Gates, Mark Zuckerberg and Jack Dorsey, and those less known for their coding exploits like Will.I.Am talking about their experiences with computer programming. Though I did not rush to coding immediately after watching the video, I downloaded a couple of tutorials, and kept the idea of learning to coding in the back of my mind. After finishing my degree in Physics, I opted to change my route and enroll in Flatiron School, as the versatility and creativity I had seen in software development was incredibly appealing to me. 
         </p>
 
         <br></br>
--- client/src/components/projectsfolder/TrackNotes.js
+++ client/src/components/projectsfolder/TrackNotes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import TrackPic from '../../images/tracknotes.png'
+import TrackPic from '../../images/trackpic.png'
 
 export class TrackNotes extends Component {
   render() {
@@ -14,25 +14,15 @@ export class TrackNotes extends Component {
         <p class="thin">
           TrackNotes was the last application that I created as a Flatiron student <small><small>:(</small></small> and served as my project for React and Redux. TrackNotes is a simple notes app for musicians, allowing a user to store notes on a particular project either to create, or as you work.
         </p>
-        <p class="center"><img src={TrackPic} width="25%" alt="TrackNotes" class="responsive-img"></img></p>
+        <p class="center"><img src={TrackPic} width="35%" alt="TrackNotes" class="responsive-img"></img></p>
 
         <p class="thin">
             The project idea was something I thought would be useful, as when I create music on my laptop using the digital audio workplace Logic Pro X, I save my project titles with names that often have nothing to do with the actual song, and thus many projects become a directionless mystery upon opening them. My idea was to create an application that can provide that direction and keep track of next steps so that great trains of thought don't get forgotten. 
         </p>
 
         <p class="thin">
-            The app itself displays a list of the 
+            The app itself displays a list of the tracks a user has inputted, that are persisted in the rails backend through AJAX calls with Thunk middleware. The project has an index page where all of the tracks are available, a new page for instantiation of new tracks, and a show page which displays the information about a particular track. The state of the react components is controlled by the track reducer, which modifies the state based on a particular case.
         </p>
-
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
 
       </div>
 
