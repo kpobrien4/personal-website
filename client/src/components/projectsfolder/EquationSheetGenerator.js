f18bb9d148d34a291db47a6c55d498ef89768c0f
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-28 10:48:58 -0500

    project links added

--- client/src/components/projectsfolder/EquationSheetGenerator.js
+++ client/src/components/projectsfolder/EquationSheetGenerator.js
@@ -5,7 +5,7 @@ export class EquationSheetGenerator extends Component {
     return (
       <div>
           <br></br>
-        <h2 class="thin project-title">Equation Sheet Generator</h2><button type="submit" class="thin btn git right links waves-effect waves-light">GitHub</button> <button type="submit" class="thin btn demo right links waves-effect waves-light">Demo</button>
+        <h2 class="thin project-title">Equation Sheet Generator</h2><a href = "https://github.com/kpobrien4/equation-sheet-generator" target="_blank" rel="noopener noreferrer"><button type="submit" class="thin btn git right links waves-effect waves-light">GitHub</button></a> <a href = "https://youtu.be/42U-VjYRJqo" target="_blank" rel="noopener noreferrer"><button type="submit" class="thin btn demo right links waves-effect waves-light">Demo</button></a> 
         <hr></hr>
         <p class="center-align skills">Skills demonstrated using Object Oriented JavaScript, AJAX, serialized JSON, HTML, CSS, Ruby on Rails</p>
         <p class="thin">
@@ -13,11 +13,14 @@ export class EquationSheetGenerator extends Component {
         </p>
 
         <p class="thin">
-            Equation Sheet Generator utilizes a JavaScript front end with a Ruby on Rails API. 
+            Equation Sheet Generator utilizes a JavaScript front end with a Ruby on Rails API. The app functions relaitvely simply, as a service to add an equation and a name of that equation to a sheet, and have it all saved in one place. The equations can then be deleted, or updated at your leisure. The data submitted in the frontend is then persisted in the backend using AJAX requests.
+        </p>
+        <p class="thin">
+            This application was my first JavaScript project, and as such was one of my more difficult challenges at Flatiron School, however the idea was something that I believed in and felt could be useful to others which allowed me to motivate myself to create it. Given the nature of the program, we had a week to work on each project and thus were only asked to create a minimum viable product, however this project still has some improvements I would love to add. 
         </p>
 
         <p class="thin">
-          Future goals and improvements on this project involve adding LaTeX equation formatting (potentially through an outside JavaScript directory), allowing each field to display information about that field on click, and formatting of the equation sheet so that it scales to an 8.5x11 piece of paper.
+          Future goals and improvements on this project involve adding LaTeX equation formatting (potentially through an outside JavaScript directory), allowing each field to display information about that field on click, providing base equations for each field, and formatting of the equation sheet so that it scales to an 8.5x11 piece of paper.
         </p>
 
 
--- client/src/components/projectsfolder/TrackNotes.js
+++ client/src/components/projectsfolder/TrackNotes.js
@@ -6,7 +6,7 @@ export class TrackNotes extends Component {
       <div>
         <div>
             <br></br>
-            <h2 class="thin project-title">TrackNotes </h2><button type="submit" class="thin btn git right links waves-effect waves-light">GitHub</button> <button type="submit" class="thin btn demo right links waves-effect waves-light">Demo</button>
+            <h2 class="thin project-title">TrackNotes </h2><a href = "https://github.com/kpobrien4/react-final-project-hr" target="_blank" rel="noopener noreferrer"><button type="submit" class="thin btn git right links waves-effect waves-light">GitHub</button></a> <a href = "https://youtu.be/Asj28Dqm43g" target="_blank" rel="noopener noreferrer"><button type="submit" class="thin btn demo right links waves-effect waves-light">Demo</button></a> 
         </div>
         <hr></hr>
         <p class="center-align skills">Skills demonstrated using React.js, Redux, Rails API, PostgreSQL, HTML, CSS, Thunk Middleware, AJAX, RESTful Routes</p>
