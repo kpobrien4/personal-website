7e549c3d7ae82199b4f4c9435b14f7b7c7e690c2
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-21 13:22:32 -0500

    update pictures

--- client/src/components/projectsfolder/TrackNotes.js
+++ client/src/components/projectsfolder/TrackNotes.js
@@ -12,7 +12,7 @@ export class TrackNotes extends Component {
         <hr></hr>
         <p class="center-align skills">Skills demonstrated using React.js, Redux, Rails API, PostgreSQL, HTML, CSS, Thunk Middleware, AJAX, RESTful Routes</p>
         <p class="thin">
-          TrackNotes was the last application that I created as a Flatiron student <small><small>:(</small></small> and served as my project for React and Redux. TrackNotes is a simple notes app for musicians, allowing a user to store notes on a particular project either to create, or as you work.
+          ProjectNotes was the last application that I created as a Flatiron student <small><small>:(</small></small> and served as my project for React and Redux. ProjectNotes is a simple notes app for musicians, allowing a user to store notes on a particular project either to create, or as you work.
         </p>
         <p class="thin"><img src={TrackPic} width="35%" alt="TrackNotes" class="responsive-img trackpic" align="left"></img></p>
 
