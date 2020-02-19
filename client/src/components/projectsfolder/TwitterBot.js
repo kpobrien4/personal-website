2a21fc80bbdd2b1eee62788bff8c57183f0dce07
Kevin O'Brien <kpobrien@buffalo.edu>
2020-02-19 15:27:35 -0500

    alibot description

--- client/src/components/projectsfolder/TwitterBot.js
+++ client/src/components/projectsfolder/TwitterBot.js
@@ -5,11 +5,14 @@ export class TwitterBot extends Component {
         return (
       <div>
         <br></br>
-          <h2 class="thin project-title">Twitter Bot</h2><a href = "https://github.com/kpobrien4/Twitter-Bot" target="_blank" rel="noopener noreferrer"><button type="submit" class=" btn git right links waves-effect waves-light">GitHub</button></a><a href = "https://twitter.com/MyBotPy" target="_blank" rel="noopener noreferrer"><button type="submit" class=" btn demo right links waves-effect waves-light">Project</button></a>  
+          <h2 class="thin project-title">Alibot</h2><a href = "https://github.com/kpobrien4/Twitter-Bot" target="_blank" rel="noopener noreferrer"><button type="submit" class=" btn git right links waves-effect waves-light">GitHub</button></a><a href = "https://twitter.com/Alibot2020" target="_blank" rel="noopener noreferrer"><button type="submit" class=" btn demo right links waves-effect waves-light">Project</button></a>  
         <hr></hr>
         <p class="center-align skills">Skills demonstrated using Python, Tweepy, Omniauth</p>
         <p class="thin">
-          My Twitter Bot application was created using Python, and initially served as a playground to explore the Twitter API, as well as the Tweepy libraries. 
+          The Alibot application was created as a joke, however it quickly proved to be an interesting challenge. After reading the book <i>The Count of Monte Cristo</i> by Alexandre Dumas, I developed an irrational fear of being framed for a crime that I haven't committed. Thus, the goal of this project was to create a twitter bot capable of absolving me from all potential crimes.
+        </p>
+        <p class="thin">
+          The bot uses the Tweepy library 
         </p>
       </div>
         )
