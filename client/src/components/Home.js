a4310b88809cff60b8d7314be70294e1a75a7793
Kevin O'Brien <kpobrien@buffalo.edu>
2021-02-09 14:24:24 -0500

    added easing to home h5

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,7 +16,7 @@ export class Home extends Component {
       <div className="">
         <h1 class="center home1"><img style={this.state.loaded ? {} : {display: 'none'}} src={fatherOfTheYear} width="250" height="250" alt="Me" onLoad={() => this.setState({loaded: true})}/></h1>
         <h2 class="center white-text thin home3">Full Stack Web Developer</h2>
-        <h5 class="center white-text thin">A developer eager to create quality software that can improve the lives of others</h5>
+        <h5 class="center quote-box white-text thin">A developer eager to create quality software that can improve the lives of others</h5>
         <br></br>
         {/* <Quotes /> */}
         {/* <hr></hr> */}
