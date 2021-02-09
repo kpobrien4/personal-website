6643a522e18302c0a69332b497f88dcf57d58921
Kevin O'Brien <kpobrien@buffalo.edu>
2021-02-09 12:52:09 -0500

    updated image to appear only when loaded

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,10 +6,24 @@ import '../home.css'
 
 
 export class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { imageStatus: "loading" };
+  }
+
+  handleImageLoaded() {
+    this.setState({ imageStatus: "loaded" });
+  }
+
+  handleImageErrored() {
+    this.setState({ imageStatus: "failed to load" });
+  }
+
   render() {
     return (
       <div className="container home">
-        <h1 class="center home1"><img src={fatherOfTheYear} width="250" height="250" alt="Me"/></h1>
+        <h1 class="center home1"><img onLoad={this.handleImageLoaded.bind(this)} src={fatherOfTheYear} width="250" height="250" alt="Me"/></h1>
         <h3 class="center white-text thin home3">Full Stack Web Developer</h3>
         <br></br>
         {/* <Quotes /> */}
